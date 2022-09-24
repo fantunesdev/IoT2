14b889c57879b0629f79d6872f2557ee74b9ea86
Fernando Antunes <f.antunes84@gmail.com>
2022-09-24 12:43:12 -0300

    Opção refatorada do segundo device

--- device.py
+++ device.py
@@ -1,31 +1,27 @@
 import time
 
 from hal import get_actual_weather
-from my_conf import CLIENT_ID1, CLIENT_ID2
+from my_conf import CLIENT_IDS
 from repository import *
-
 
 temperature, humidity = get_actual_weather()
 
-client1 = connect(CLIENT_ID1)
-client2 = connect(CLIENT_ID2)
+clients = []
 
+for client_id in CLIENT_IDS:
+    client = connect(client_id)
+    clients.append(client)
 
-client1.on_message = handle_message
-client1.subscribe(get_value_reciever_topic(CLIENT_ID1, 2))
-client1.loop_start()
-
-
-client2.on_message = handle_message
-client2.subscribe(get_value_reciever_topic(CLIENT_ID2, 2))
-client2.loop_start()
+    client.on_message = handle_message
+    client.subscribe(get_value_reciever_topic(client_id, 2))
+    client.loop_start()
 
 while True:
-    client1.publish(get_value_sender_topic(CLIENT_ID1, 0), temperature)
-    client1.publish(get_value_sender_topic(CLIENT_ID1, 1), humidity)
-    client2.publish(get_value_sender_topic(CLIENT_ID2, 0), temperature)
-    client2.publish(get_value_sender_topic(CLIENT_ID2, 1), humidity)
+    for client in clients:
+        client_id = client._client_id.decode('UTF-8')
+        client.publish(get_value_sender_topic(client_id, 0), temperature)
+        client.publish(get_value_sender_topic(client_id, 1), humidity)
     temperature, humidity = handle_temperature(temperature, humidity)
-    time.sleep(10)
+    time.sleep(3)
 
 # client.disconnect()
--- my_conf.py
+++ my_conf.py
@@ -1,6 +1,5 @@
 USER = 'c14f67e0-3b81-11ed-bf0a-bb4ba43bd3f6'
 PASSWORD = '8e047b0f5d314b994969fae6b0971c1e42a57353'
-CLIENT_ID1 = '232380f0-3b82-11ed-baf6-35fab7fd0ac8'
-CLIENT_ID2 = '6c228110-3c0f-11ed-baf6-35fab7fd0ac8'
+CLIENT_IDS = ['232380f0-3b82-11ed-baf6-35fab7fd0ac8', '6c228110-3c0f-11ed-baf6-35fab7fd0ac8']
 SERVER = 'mqtt.mydevices.com'
 PORT = 1883
